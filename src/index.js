118e8b271af88eaaf77b65c4b175ee37270eee52
Devgupta0 <emailidabc3>
2020-12-02 16:03:31 +0530

    calculator

--- src/index.js
+++ src/index.js
@@ -15,45 +15,118 @@ app.get("",(req,res)=>{
     res.send("Hello World");
 });
 app.post("/add",(req,res)=>{
-
-    const num1 = Number(req.body.num1);
-    const num2  =  Number(req.body.num2);
-    const sum1 = num1+num2;
-    res.status(201);
-    res.send({
-        message: "the sum of given two numbers",
-        sum: sum1
-        });
+        const num1 = req.body.num1;
+        const num2 = req.body.num2;
+        if(typeof(num1) != "number" || typeof(num2) != "number"){
+            res.send({
+                status: 'error',
+                message: "Invalid data types"
+            });
+        }else{
+            const sum1 = num1+num2;
+            if(diff >= 999999 || num1>=999999 || num2>=999999){
+                res.send({
+                    status: 'error',
+                    message: "Overflow",
+                });
+            }else if(diff <= -999999 || num1 <= -999999 || num2 <= -999999){
+                res.send({
+                    status: 'error',
+                    message: "Underflow"
+                });
+            }else{
+                res.status(200).send({
+                message: "the sum of given two numbers",
+                sum: sum1
+            });
+        }
+    }
 });
+        
 app.post("/sub",(req,res)=>{
-    const num1 = Number(req.body.num1);
-    const num2 = Number(req.body.num2);
-    const diff = num1-num2;
-    res.status(201);
-    res.send({
-        message: "the difference of given two numbers",
-        difference: diff
-    });
+    const num1 = req.body.num1;
+    const num2 = req.body.num2;
+    if(typeof(num1) != "number" || typeof(num2) != "number"){
+        res.send({
+            status: 'error',
+            message: "Invalid data types"
+        });
+    }else{
+        const diff = num1-num2;
+        if(diff >= 999999 || num1>=999999 || num2>=999999){
+            res.send({
+                status: 'error',
+                message: "Overflow",
+            });
+        }else if(diff <= -999999 || num1 <= -999999 || num2 <= -999999){
+            res.send({
+                status: 'error',
+                message: "Underflow"
+            });
+        }else{
+            res.status(200).send({
+                status: 'success',
+                message: "the difference of given two number",
+                difference: diff
+            });
+        }
+    }
 });
 app.post("/multiply",(req,res)=>{
-    const num1 = Number(req.body.num1);
-    const num2 = Number(req.body.num2);
-    const mul = num1*num2;
-    res.status(201);
-    res.send({
-        message: "The product of given numbers",
-        result: mul
-    });
+    const num1 = req.body.num1;
+    const num2 = req.body.num2;
+    if(typeof(num1) != "number" || typeof(num2) != "number"){
+        res.send({
+            status: 'error',
+            message: "Invalid data types"
+        });
+    }else{
+        const mul = num1*num2;
+        if(mul >= 999999 || num1 >= 999999 || num2 >= 999999){
+            res.send({
+                status: 'error',
+                message: "Overflow"
+            });
+        }else if(mul <= -999999 || num1 <= -999999 || num2 <= -999999){
+            res.send({
+                status: 'error',
+                message: "Underflow"
+            });
+        }else{
+            res.status(200).send({
+                message: "The product of given numbers",
+                result: mul
+            });
+        }
+    }
 });
 app.post("/divide",(req,res)=>{
-    const num1 = Number(req.body.num1);
-    const num2 = Number(req.body.num2);
-    const div = num1/num2;
-    res.status(201);
-    res.send({
-        message: "The division of given numbers",
-        result: div
-    });
+    const num1 = req.body.num1;
+    const num2 = req.body.num2;
+    if(typeof(num1) != "number" || typeof(num2) != "number"){
+        res.send({
+            status: 'error',
+            message: "Invalid data types"
+        });
+    }else{
+        const div = num1/num2;
+        if(div >= 999999 || num1 >= 999999 || num2 >= 999999){
+            res.send({
+                status: 'error',
+                message: "Overflow"
+            });
+        }else if(div <= -999999 || num1 <= -999999 || num2 <= -999999){
+            res.send({
+                status: 'error',
+                message: "Underflow"
+            });
+        }else{
+            res.status(200).send({
+                message: "The division of given numbers",
+                result: div
+            });
+        }
+    }
 });
 // your code goes here
 
